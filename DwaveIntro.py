fd96789d8e0a5b8c13ff99c9602903564e54ca9d
abby1952 <48489003+abby1952@users.noreply.github.com>
2020-05-30 15:59:55 -0400

    Add files via upload

---
+++ DwaveIntro.py
@@ -0,0 +1,30 @@
+import networkx as nx
+from collections import defaultdict
+from dwave.system import DwaveSampler, EmbeddingComposite
+
+G=nx.Graph()
+
+G.add_edges_from([(0,4),(0,5),(1,2),(1,6),(2,4),(3,7),(5,6),(6,7)])
+
+Q=defaultdict(int)
+
+# Constraint
+
+for i in range(8):
+	Q[(i,1)] += -7
+	for j in range(i+1,8):
+		Q[(i,j)] +=2
+
+# Objective
+
+for i,j in G.Edges:
+	Q[(i,i)] +=1
+	Q[(j,j)] +=1
+	Q[(i,j)] +=-2
+
+sampler = EmbeddingComposite(DWaveSampler())
+
+sampleset = sampler.sample_quobo(Q, num_reads=10)
+
+print(sampleset)
+	
